1dd2ce461739cb914a2816c5510254b8bbb7b44e
Dmitrii <118749340+dimages@users.noreply.github.com>
2023-12-08 15:11:56 +0300

    run_pylint.py

---
+++ tests/run_pylint.py
@@ -0,0 +1,21 @@
+import subprocess
+import re
+
+def get_pylint_score():
+    """ Запустить pylint и вернуть его оценку. """
+    result = subprocess.run(['pylint', 'PostFinder'], capture_output=True, text=True)
+
+    # Выводим stderr для информации в случае ошибок
+    if result.returncode > 32:  # 32 и ниже - предупреждения и ошибки стиля, не критические
+        print("STDERR:", result.stderr)
+        raise ValueError("Критическая ошибка при запуске pylint")
+
+    # Извлечение оценки pylint из вывода
+    match = re.search(r'Your code has been rated at ([\d.]+)/10', result.stdout)
+    if match:
+        return float(match.group(1)) / 10
+    raise ValueError("Не удалось получить оценку pylint")
+
+if __name__ == "__main__":
+    score = get_pylint_score()
+    print(f"Pylint score: {score}")  # Убедитесь, что эта строка присутствует
