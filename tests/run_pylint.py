05659ca30bf5017b82db6a370eea625d21426dc7
Dmitrii <118749340+dimages@users.noreply.github.com>
2023-12-09 12:55:52 +0300

    Add files via upload

--- tests/run_pylint.py
+++ tests/run_pylint.py
@@ -6,13 +6,15 @@ def get_pylint_score() -> Tuple[float, str]:
     """ Run pylint and return its score and output. """
     result = subprocess.run(['pylint', 'PostFinder'], capture_output=True, text=True)
 
-    # Print stderr for information in case of errors
-    if result.returncode > 32:  # 32 and below - warning and style errors, not critical
-        print("STDERR:", result.stderr)
-        raise ValueError("Critical error when starting pylint")
+    # Assert non-critical errors
+    assert result.returncode <= 32, f"Critical error when starting pylint: {result.stderr}"
 
     # Extracting pylint score from output
     match = re.search(r'Your code has been rated at ([\d.]+)/10', result.stdout)
-    if match:
-        return float(match.group(1)) / 10, result.stdout
-    raise ValueError("Failed to get pylint evaluation")
+    assert match, "Failed to get pylint evaluation"
+
+    return float(match.group(1)) / 10, result.stdout
+
+if __name__ == "__main__":
+    score, output = get_pylint_score()
+    print(f"Pylint score: {score}")
