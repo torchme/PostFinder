2b5a8e3e05ed8ce8a09d7e797026c47fc82d60fb
Dmitrii <118749340+dimages@users.noreply.github.com>
2023-12-09 12:45:22 +0300

    Add files via upload

--- tests/run_pylint.py
+++ tests/run_pylint.py
@@ -1,21 +1,21 @@
 import subprocess
 import re
 
-def get_pylint_score() -> float:
-    """ Запустить pylint и вернуть его оценку. """
+def get_pylint_score():
+    """ Run pylint and return its score and output. """
     result = subprocess.run(['pylint', 'PostFinder'], capture_output=True, text=True)
 
-    # Выводим stderr для информации в случае ошибок
-    if result.returncode > 32:  # 32 и ниже - предупреждения и ошибки стиля, не критические
+    # Print stderr for information in case of errors
+    if result.returncode > 32:  # 32 and below - warning and style errors, not critical
         print("STDERR:", result.stderr)
-        raise ValueError("Критическая ошибка при запуске pylint")
+        raise ValueError("Critical error when starting pylint")
 
-    # Извлечение оценки pylint из вывода
+    # Extracting pylint score from output
     match = re.search(r'Your code has been rated at ([\d.]+)/10', result.stdout)
     if match:
-        return float(match.group(1)) / 10
-    raise ValueError("Не удалось получить оценку pylint")
+        return float(match.group(1)) / 10, result.stdout
+    raise ValueError("Failed to get pylint evaluation")
 
 if __name__ == "__main__":
-    score = get_pylint_score()
-    print(f"Pylint score: {score}")  # Убедитесь, что эта строка присутствует
+    score, output = get_pylint_score()
+    print(f"Pylint score: {score}")
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,6 @@
 from .run_pylint import get_pylint_score
 
 def test_pylint_quality():
-    threshold = 0.75  # Установите пороговое значение качества кода
-    score = get_pylint_score()
-    assert score >= threshold, f"Качество кода ({score}) ниже порога ({threshold})"
-
-
-
-
-
+    threshold = 0.75  # Set the code quality threshold
+    score, output = get_pylint_score()
+    assert score >= threshold, f"Code quality ({score}) is below the threshold ({threshold}).\nDetails:\n{output}"
