e9b4d039a2f8bb684c29f018090f8b0416666571
Sokolov Fedor <sokol_f4@mail.ru>
2023-12-28 22:33:09 +0300

    Fix mypy config and bugs with tests

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 
 dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
@@ -10,5 +10,5 @@ load_dotenv(dotenv_path=dotenv_path)
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 PROXY_API_KEY = os.getenv("PROXY_API_KEY")
