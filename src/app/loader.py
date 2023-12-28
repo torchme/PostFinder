5b72f064ee424f9048a4cc959bfbb51639c9dcb6
Sokolov Fedor <sokol_f4@mail.ru>
2023-12-29 00:58:19 +0300

    Add command for scrapping channels

--- src/app/loader.py
+++ src/app/loader.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
+from telethon import TelegramClient
 
-from src.config import TELEGRAM_BOT_TOKEN
+from src.config import API_HASH, API_ID, TELEGRAM_BOT_TOKEN
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher()
+client = TelegramClient("post_finder", api_id=API_ID, api_hash=API_HASH)
