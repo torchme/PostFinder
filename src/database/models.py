7e956bef5f38b0c25abf9d9d88a6e2e0fd279405
Sokolov Fedor <sokol_f4@mail.ru>
2024-01-18 17:59:39 +0300

    Added 'action' table and new db migrations

--- src/database/models.py
+++ src/database/models.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = "user"
 
     user_id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, nullable=False)
+    telegram_id = Column(Integer, nullable=False, unique=True)
     username = Column(String, nullable=True)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
@@ -29,7 +29,21 @@ class UserSubscription(Base):
     __tablename__ = "user_subscription"
 
     subscription_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.user_id"))
+    telegram_id = Column(Integer, ForeignKey("user.telegram_id"))
     type_id = Column(Integer, ForeignKey("subscription_type.type_id"))
     valid_from = Column(TIMESTAMP, nullable=False)
     valid_to = Column(TIMESTAMP, nullable=False)
+
+
+class Action(Base):
+    __tablename__ = "action"
+
+    action_id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, ForeignKey("user.telegram_id"))
+    platform_type = Column(String, nullable=True)
+    resouce_name = Column(String, nullable=True)
+    query = Column(String, nullable=True)
+    response = Column(String, nullable=True)
+    input_tokens = Column(Integer, nullable=True)
+    output_tokens = Column(Integer, nullable=True)
+    feedback = Column(String, nullable=True)
