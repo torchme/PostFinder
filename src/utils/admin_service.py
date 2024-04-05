c2ab316b9bb2e9a4194231ed01ec6078a2c2d4e0
umbilnm <kill4ik2002@gmail.com>
2024-04-05 23:40:23 +0300

    Fix adminchat id

--- src/utils/admin_service.py
+++ src/utils/admin_service.py
@@ -1,12 +1,12 @@
 from src.app.loader import bot
-from src.config import config
+from src.config import config, ADMIN_CHAT_ID
 from src.utils.markup import inline_markup_admin
 
 
 async def send_to_admins(user_id: int, username: str, first_name: str, last_name: str):
     if user_id not in config.whitelist:
         await bot.send_message(
-            config.admin_ids,
+            ADMIN_CHAT_ID,
             f"user_id: {user_id}\nusername: {username}\nfirst_name: {first_name}\nlast_name: {last_name}\n\nРазрешить доступ?",
             reply_markup=inline_markup_admin(user_id=user_id, username=username),
             parse_mode=None,
